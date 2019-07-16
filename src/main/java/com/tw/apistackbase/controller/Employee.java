1c5a0e060c7c9d16d5815d4dd66794d953663ffa
MrLinZHui <742847492@qq.com>
2019-07-16 19:57:42 +0800

    should_return_200_when_get_Employees

---
+++ src/main/java/com/tw/apistackbase/controller/Employee.java
@@ -0,0 +1,51 @@
+package com.tw.apistackbase.controller;
+
+public class Employee {
+
+    private int age;
+    private int id;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String name;
+    private String gender;
+    public Employee(int id, String name, int age, String gender) {
+        this.id = id;
+        this.name =name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Employee() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
--- src/main/java/com/tw/apistackbase/controller/HelloResource.java
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -1,27 +1,59 @@
 package com.tw.apistackbase.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
  * Created by jxzhong on 18/08/2017.
  */
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/employees")
 public class HelloResource {
-
+    private List<Employee> employeeList = new ArrayList();
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
-    @GetMapping(path = "/{userName}", produces = {"application/json"})
-    public ResponseEntity<String> getAll(@PathVariable String userName) {
-
-        return ResponseEntity.ok("Hello:" + userName);
+    //    @GetMapping(path = "/{userName}", produces = {"application/json"})
+//    public ResponseEntity<String> getAll(@PathVariable String userName) {
+//
+//        return ResponseEntity.ok("Hello:" + userName);
+//    }
+    @GetMapping("")
+    public ResponseEntity getAll(){
+        employeeList.add(new Employee(0,"zhangsan",18,"male"));
+        return ResponseEntity.ok().body(employeeList);
     }
 
+    @PostMapping
+    public ResponseEntity created(@RequestBody Employee employee){
+        employeeList.add(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
+    @PutMapping
+    public ResponseEntity nodify(@RequestBody Employee employee){
+        for(int i = 0; i<employeeList.size();i++){
+            if(employeeList.get(i).getId()==employee.getId()){
+                employeeList.get(i).setAge(employee.getAge());
+                employeeList.get(i).setGender(employee.getGender());
+                employeeList.get(i).setName(employee.getName());
+            }
+            break;
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity deleteEmployee(@PathVariable int id){
+        for(int i = 0; i<employeeList.size();i++){
+            if(employeeList.get(i).getId()== id){
+                employeeList.remove(employeeList.get(i));
+            }
+        }
+        return ResponseEntity.ok(employeeList);
+    }
 }
